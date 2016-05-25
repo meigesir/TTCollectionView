64a1e1daa5f6c9acce5de66770227d949350c7df
tianmei.li <tianmei.li@husor.com>
2016-05-25 15:24:10 +0800

    Initial Commit

---
+++ TTCollectionView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TTCollectionView
+//
+//  Created by phenix on 5/25/16.
+//  Copyright © 2016 phenix. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TTCollectionView/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  TTCollectionView
+//
+//  Created by phenix on 5/25/16.
+//  Copyright © 2016 phenix. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
