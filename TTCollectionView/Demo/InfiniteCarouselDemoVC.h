fdde89f214d86baf90a1f4d472bce840a39159a6
meigesir <emomeild@gmail.com>
2016-05-28 23:17:34 +0800

    Added：添加无限轮播、瀑布流的入口

---
+++ TTCollectionView/Demo/InfiniteCarouselDemoVC.h
@@ -0,0 +1,13 @@
+//
+//  InfiniteCarouselDemoVC.h
+//  TTCollectionView
+//
+//  Created by phenix on 5/28/16.
+//  Copyright © 2016 phenix. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface InfiniteCarouselDemoVC : UIViewController
+
+@end
--- TTCollectionView/Demo/InfiniteCarouselDemoVC.m
+++ TTCollectionView/Demo/InfiniteCarouselDemoVC.m
@@ -1,34 +1,36 @@
 //
-//  ViewController.m
+//  InfiniteCarouselDemoVC.m
 //  TTCollectionView
 //
-//  Created by phenix on 5/25/16.
+//  Created by phenix on 5/28/16.
 //  Copyright © 2016 phenix. All rights reserved.
 //
 
-#import "ViewController.h"
+#import "InfiniteCarouselDemoVC.h"
 #import "TTMInfiniteCarouselView.h"
 
-@interface ViewController ()
+@interface InfiniteCarouselDemoVC ()
 
 @property (nonatomic, strong) TTMInfiniteCarouselView *infiniteCarouselView;
 
 @end
 
-@implementation ViewController
+@implementation InfiniteCarouselDemoVC
+
+- (void)awakeFromNib
+{
+    [self.view addSubview:self.infiniteCarouselView];
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
-    
-    [self.view addSubview:self.infiniteCarouselView];
+    // Do any additional setup after loading the view
     
     [self.infiniteCarouselView updateWithDataArray:@[@{@"img": @"1_750x300.jpg"}, @{@"img": @"2_750x300.jpg"}, @{@"img": @"3_750x300.jpg"}, @{@"img": @"4_750x300.jpg"}, @{@"img": @"5_750x300.jpg"}]];
     
-    [self.infiniteCarouselView startTimer];
-    
     [self.infiniteCarouselView reloadData];
     
+    [self.infiniteCarouselView startTimer];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -37,12 +39,12 @@
 }
 
 - (TTMInfiniteCarouselView *)infiniteCarouselView {
-	if(_infiniteCarouselView == nil) {
+    if(_infiniteCarouselView == nil) {
         _infiniteCarouselView = [[TTMInfiniteCarouselView alloc] initWithFrame:(CGRect){CGPointZero, [UIScreen mainScreen].bounds.size.width, [UIScreen mainScreen].bounds.size.width / 2}];
         _infiniteCarouselView.pageControlAlignment = TTMInfiniteCarouselViewPageControlAlignmentCenter;
         _infiniteCarouselView.currentPageIndicatorTintColor = [UIColor purpleColor];
-	}
-	return _infiniteCarouselView;
+    }
+    return _infiniteCarouselView;
 }
 
 @end
